507c9ead5ed41bee66fb5484b7322739c10f5f96
chanm0 <mikec201722@gmail.com>
2019-02-07 20:08:34 -0800

    check permuation

--- chapter_1_arrays_and_strings/chapter_1_section_1_is_unique/IsUnique.java
+++ chapter_1_arrays_and_strings/chapter_1_section_1_is_unique/IsUnique.java
@@ -2,11 +2,13 @@ class IsUnique{
 
     public static void main(String[]args){
 
-        System.out.println(IsUnique("abc"));
-        System.out.println(IsUnique("abcdc"));
+        System.out.println(Is_Unique("abc"));
+        System.out.println(Is_Unique("abcdc"));
+        System.out.println(Check_Permuation("abcdc","accdb"));
+        System.out.println(Check_Permuation("abcdc","dccbe"));
     }
 
-    static boolean IsUnique(String str)
+    static boolean Is_Unique(String str)
     {
         if (str.length() > 128){
             return false;
@@ -20,6 +22,28 @@ class IsUnique{
             asciiAlphabet[str.charAt(i)] = true;
         }
         return true;
+    }
+
+    static boolean Check_Permuation(String str1, String str2)
+    {
+        if( str1.length() != str2.length()){
+            return false;
+        }
+
+        int[] char_count = new int[128];
+        for(int i = 0; i < str1.length(); i++){
+            char_count[str1.charAt(i)]++;
+        }
+
+        for(int j = 0; j < str2.length(); j++){
+            char_count[str2.charAt(j)]--;
+            if(char_count[str2.charAt(j)] < 0){
+                return false;
+            }
+        }
+
+        return true;
+
     }
 
 
